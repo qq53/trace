811f997f5b5bd02ffd01afe361d237c34b13809c
qq53 <vap0r@vap0r.cn>
2015-11-08 02:37:35 +0800

    inf

--- static/js/index.js
+++ static/js/index.js
@@ -43,7 +43,7 @@ function uploadProgress(evt) {
 
 function uploadComplete(evt) {
     /* This event is raised when the server send back a response */
-    alert(evt.target.responseText);
+	$('#contents').html(evt.target.responseText)
 }
 
 function uploadFailed(evt) {
