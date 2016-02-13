9e3deccff44226e6131a1f3eb5906b2154e85ed4
qq53 <vap0r@vap0r.cn>
2016-02-14 00:36:07 +0800

    ups

---
+++ index.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from jinja2 import Environment, FileSystemLoader
+import os
+import codecs
+
+def make():
+	cwd = os.path.split(os.path.realpath(__file__))[0] + '/'
+
+	env = Environment(loader = FileSystemLoader(cwd+'templates'))
+	template_index = env.get_template('index.html')
+	
+	with codecs.open(cwd+'index.html', 'w', 'utf-8') as f:
+		f.write(template_index.render())
+
+if __name__ == '__main__':
+	make()
